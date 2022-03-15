fc9123a4e453937fc07aac09d9fb90363dfc889d
Jetinat0r <jetin8t0r@gmail.com>
2022-03-15 00:21:30 -0400

    Unlock Doors
    - Added a check to see if the player has all of the required items for a given WorldNodeConnection (i.e. there's a locked door, but you pick up a key so it lets you through)
    - Updated the player prefab to fix some issues related to physics

--- Lost & Found/Assets/Scripts/Game Scripts/TransitionObject.cs
+++ Lost & Found/Assets/Scripts/Game Scripts/TransitionObject.cs
@@ -11,33 +11,60 @@ public class TransitionObject : InteractionTarget
         WorldNodeConnector _connector = SceneController.instance.GetConnectorFromTitle(connectionTitle);
         if (_connector.isLocked)
         {
-            //Create new dialogue
-            DialogueScriptableObject _dialogue = ScriptableObject.CreateInstance("DialogueScriptableObject") as DialogueScriptableObject;
+            //See if the player has the items to open the door
+            bool _hasReqItems = true;
+            foreach(string _reqItem in _connector.reqItems)
+            {
+                bool _hasThisItem = false;
+                foreach(QuestItemScriptableObject _heldItem in PlayerInventory.instance.curHeldItems)
+                {
+                    if(_reqItem == _heldItem.idItemName)
+                    {
+                        _hasThisItem = true;
+                        break;
+                    }
+                }
 
-            //Add text
-            _dialogue.dialogueText = new List<string>();
-            _dialogue.dialogueText.Add(_connector.lockedText);
+                if (!_hasThisItem)
+                {
+                    _hasReqItems = false;
+                    break;
+                }
+            }
 
-            //Add mood
-            _dialogue.moodsForLines = new List<PortraitMood>();
-            _dialogue.moodsForLines.Add(PortraitMood.None);
+            //Make a dialogue saying that the door can't be opened
+            if (!_hasReqItems)
+            {
+                //Create new dialogue
+                DialogueScriptableObject _dialogue = ScriptableObject.CreateInstance("DialogueScriptableObject") as DialogueScriptableObject;
 
-            //"Add" portraits
-            _dialogue.SetNpcPortraits(ScriptableObject.CreateInstance("NpcPortraitsScriptableObject") as NpcPortraitsScriptableObject);
+                //Add text
+                _dialogue.dialogueText = new List<string>();
+                _dialogue.dialogueText.Add(_connector.lockedText);
 
-            //Play Dialogue (and tell it to re-enable player movement)
-            DialogueManager.instance.StartDialogue(_dialogue, "", GameManager.instance.EnablePlayerInput, itPostInteractEvents);
+                //Add mood
+                _dialogue.moodsForLines = new List<PortraitMood>();
+                _dialogue.moodsForLines.Add(PortraitMood.None);
+
+                //"Add" portraits
+                _dialogue.SetNpcPortraits(ScriptableObject.CreateInstance("NpcPortraitsScriptableObject") as NpcPortraitsScriptableObject);
+
+                //Play Dialogue (and tell it to re-enable player movement)
+                DialogueManager.instance.StartDialogue(_dialogue, "", GameManager.instance.EnablePlayerInput, itPostInteractEvents);
+
+                return;
+            }
         }
-        else
-        {
-            //Create a delegate
-            SceneController.RunOnSceneLoad _runOnSceneLoad = null;
 
-            //Assign methods to delegate
-            //_runOnSceneLoad += GameManager.instance.EnablePlayerMovement;
+        #region Start Transition
+        //Create a delegate
+        SceneController.RunOnSceneLoad _runOnSceneLoad = null;
 
-            //Attempt transition
-            SceneController.instance.MoveThroughConnection(connectionTitle, _runOnSceneLoad);
-        }
+        //Assign methods to delegate
+        //_runOnSceneLoad += GameManager.instance.EnablePlayerMovement;
+
+        //Attempt transition
+        SceneController.instance.MoveThroughConnection(connectionTitle, _runOnSceneLoad);
+        #endregion
     }
 }
